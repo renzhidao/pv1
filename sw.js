9fa8132bbc7798b1ddb74315ed6e999b9b237037
renzhidao <45433332+renzhidao@users.noreply.github.com>
2025-11-29 00:44:19 +0800

    Auto Backup: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'p1-cache-v3';
+const CACHE_NAME = 'p1-v10-final';
 const ASSETS = [
   './',
   './index.html',
@@ -7,12 +7,5 @@ const ASSETS = [
   'https://cdn.jsdelivr.net/npm/peerjs@1.5.0/dist/peerjs.min.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
